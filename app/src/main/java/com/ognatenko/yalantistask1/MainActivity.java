4f903c99d9c602d355ef7b5c7a29646baf9308eb
Olga Gnatenko <lady.irony.ua@gmail.com>
2016-03-29 05:33:30 +0300

    Minor improvements
    Minor improvements (layout, download)

--- app/src/main/java/com/ognatenko/yalantistask1/MainActivity.java
+++ app/src/main/java/com/ognatenko/yalantistask1/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setHeader();
+        setRecyclerView();
     }
 
     public void onClick(View view) {
